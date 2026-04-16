49c6a6d3b6b832d57fc4bffae45cbb1a5435b460
*Ahmed <*ahmedayman77770@gmail.com*>
2026-04-16 15:45:32 +0200

    first version of receptionist

--- model/Receptionist.java
+++ model/Receptionist.java
@@ -1,4 +1,22 @@
 package model;
 
-public class Receptionist {
+public class Receptionist extends Staff {
+    public Receptionist(String username, String password, String dateOfBirth, int workingHours) {
+        super(username, password, dateOfBirth, workingHours);
+    }
+    
+    @Override
+    public void performActions() {
+        // code to perform receptionist-specific actions
+        System.out.println("Performing receptionist actions...");
+    }
+
+    public void checkInGuest(){
+        System.out.println("Checking in a guest...");
+    }
+    public void checkOutGuest(){
+            System.out.println("Checking out a guest...");
+        }
+
+
 }
