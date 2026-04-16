480ef9fdc01832124448308c9c3e4daf7f666d11
*Ahmed <*ahmedayman77770@gmail.com*>
2026-04-16 16:21:30 +0200

    managable interface added to admin class

--- model/Admin.java
+++ model/Admin.java
@@ -1,9 +1,9 @@
 package model;
 import enums.Role;
 import database.HotelDatabase;
-
+import interfaces.Manageable;
 // Admin class that extends Staff
-public class Admin extends Staff {
+public class Admin extends Staff implements Manageable {
     // Constructor for Admin
     public Admin(String username, String password, String dateOfBirth, int workingHours) {
         super(username, password, dateOfBirth, workingHours, Role.ADMIN);
@@ -15,19 +15,22 @@ public class Admin extends Staff {
         System.out.println("Performing admin actions...");
     }
     // Method to add a new room
+    @Override
     public void addRoom(Room room){
         System.out.println("Adding a new room...");
          HotelDatabase.rooms.add(room);
          //rooms is the arraylist of room in the database class, will be fixed when database class is finised
     }
     // Method to remove a room
-    public void removeRoom(Room room){
+    @Override
+    public void deleteRoom(Room room){
         System.out.println("Removing a room...");
-        HotelDatabase.room.remove(room);
+        HotelDatabase.rooms.remove(room);
         //rooms is the arraylist of room in the database class, will be fixed when database class is finised
     }
     // Method to update room information
-    public void UpdateRoom(){
+    @Override
+    public void updateRoom(Room room){
         System.out.println("Updating room information...");
     }
     // Method to read room information
