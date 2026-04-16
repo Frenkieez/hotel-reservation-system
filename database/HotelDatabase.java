7e200224b7a9b694da93337129c1e1890795ab15
*Ahmed <*ahmedayman77770@gmail.com*>
2026-04-16 22:59:58 +0200

    temporary version of the database

--- database/HotelDatabase.java
+++ database/HotelDatabase.java
@@ -1,4 +1,9 @@
 package database;
+import java.util.ArrayList;
+import model.Room;
+import model.Reservation;
 
 public class HotelDatabase {
+    public static ArrayList<Room> rooms = new ArrayList<>();
+    public static ArrayList<Reservation> reservations = new ArrayList<>();
 }
