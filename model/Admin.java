07772adbda7bd022949dcab3f8a8e446226b781f
*Ahmed <*ahmedayman77770@gmail.com*>
2026-04-16 15:50:11 +0200

    comments added to admin class

--- model/Admin.java
+++ model/Admin.java
@@ -2,29 +2,35 @@ package model;
 
 import database.HotelDatabase;
 
+// Admin class that extends Staff
 public class Admin extends Staff {
+    // Constructor for Admin
     public Admin(String username, String password, String dateOfBirth, int workingHours) {
         super(username, password, dateOfBirth, workingHours);
     }
-
+// Override the performActions method to provide admin-specific functionality
     @Override
     public void performActions() {
         // code to perform admin-specific actions
         System.out.println("Performing admin actions...");
     }
+    // Method to add a new room
     public void addRoom(Room room){
         System.out.println("Adding a new room...");
          HotelDatabase.rooms.add(room);
          //rooms is the arraylist of room in the database class, will be fixed when database class is finised
     }
+    // Method to remove a room
     public void removeRoom(Room room){
         System.out.println("Removing a room...");
         HotelDatabase.room.remove(room);
         //rooms is the arraylist of room in the database class, will be fixed when database class is finised
     }
+    // Method to update room information
     public void UpdateRoom(){
         System.out.println("Updating room information...");
     }
+    // Method to read room information
     public void readRoom(){
         System.out.println("Reading room information...");
     }
