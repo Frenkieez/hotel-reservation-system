6c020b295347b1e1a723e1b6c586e74c5310d14d
*Ahmed <*ahmedayman77770@gmail.com*>
2026-04-17 18:13:17 +0200

    temmporary Datatbase 2

--- database/HotelDatabase.java
+++ database/HotelDatabase.java
@@ -1,9 +1,11 @@
 package database;
 import java.util.ArrayList;
 import model.Room;
+import model.Guest;
 import model.Reservation;
 
 public class HotelDatabase {
     public static ArrayList<Room> rooms = new ArrayList<>();
     public static ArrayList<Reservation> reservations = new ArrayList<>();
+    public static ArrayList<Guest> guests = new ArrayList<>();
 }
