12e33bb200a9b5f7916066efb7c6e562b474ec03
Frenkieez <frenkieez21@gmail.com>
2026-04-19 01:09:02 +0200

    Add all RoomType class components, Use new custom exceptions

--- model/RoomType.java
+++ model/RoomType.java
@@ -1,4 +1,39 @@
 package model;
 
+import exceptions.InvalidPriceException;
+
 public class RoomType {
+
+    private String name;
+    private double pricePerNight;
+
+    // Constructor
+    public RoomType(String name, double pricePerNight) throws InvalidPriceException {
+        this.name = name;
+
+        if (pricePerNight <= 0) {
+            throw new InvalidPriceException("Price per night must be greater than 0.");
+        }
+
+        this.pricePerNight = pricePerNight;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public double getPricePerNight() {
+        return pricePerNight;
+    }
+
+    // Setter with validation
+    public void setPricePerNight (double pricePerNight) throws InvalidPriceException{
+        if (pricePerNight <= 0) {
+            throw new InvalidPriceException("Price per night must be greater than 0.");
+        }
+
+        this.pricePerNight = pricePerNight;
+    }
 }
+
