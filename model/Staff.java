1e871bebc2e120c91f1103036bff3eceec636d31
Frenkieez <frenkieez21@gmail.com>
2026-04-18 21:39:57 +0200

    Add temporary classes more detailed version

--- model/Staff.java
+++ model/Staff.java
@@ -1,4 +1,31 @@
 package model;
 
-public class Staff {
+import enums.Role;
+import java.time.LocalDate;
+
+/*
+ * TEMPORARY VERSION of Staff class
+ * Used until we integrate full real classes
+ */
+public abstract class Staff {
+
+    protected String username;
+    protected String password;
+    protected LocalDate dateOfBirth;
+    protected int workingHours;
+    protected Role role;
+
+    public Staff(String username, String password, LocalDate dateOfBirth, int workingHours, Role role) {
+        this.username = username;
+        this.password = password;
+        this.dateOfBirth = dateOfBirth;
+        this.workingHours = workingHours;
+        this.role = role;
+    }
+
+    public String getUsername() { return username; }
+
+    public Role getRole() { return role; }
+
+    public abstract void performDuties();
 }
