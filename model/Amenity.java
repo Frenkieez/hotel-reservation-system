dc8fc2d559558580922f767b7d557f2a10720fc2
Frenkieez <frenkieez21@gmail.com>
2026-04-19 01:08:31 +0200

    Add all Amenity class components

--- model/Amenity.java
+++ model/Amenity.java
@@ -1,4 +1,15 @@
 package model;
 
 public class Amenity {
+    private String name;
+
+    // Constructor
+    public Amenity(String name) {
+        this.name = name;
+    }
+
+    // Getter
+    public String getName() {
+        return name;
+    }
 }
