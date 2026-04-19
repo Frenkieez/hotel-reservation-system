64e2ef8c35750bec65ed2052ee361708f298976c
*Ahmed <*ahmedayman77770@gmail.com*>
2026-04-19 20:54:48 +0200

    Database 3

--- database/HotelDatabase.java
+++ database/HotelDatabase.java
@@ -1,6 +1,8 @@
 package database;
 import java.util.ArrayList;
 import model.Room;
+import model.RoomType;
+import model.Amenity;
 import model.Guest;
 import model.Reservation;
 
@@ -8,4 +10,6 @@ public class HotelDatabase {
     public static ArrayList<Room> rooms = new ArrayList<>();
     public static ArrayList<Reservation> reservations = new ArrayList<>();
     public static ArrayList<Guest> guests = new ArrayList<>();
+    public static ArrayList<Amenity> amenities = new ArrayList<>();
+     public static ArrayList<RoomType> roomTypes = new ArrayList<>();
 }
