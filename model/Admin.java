8b2c2e40d852bff345550f43cb543aee67e51250
*Ahmed <*ahmedayman77770@gmail.com*>
2026-04-19 20:55:23 +0200

    second version of admin class

--- model/Admin.java
+++ model/Admin.java
@@ -14,17 +14,16 @@ public class Admin extends Staff implements Manageable {
         // code to perform admin-specific actions
         System.out.println("Performing admin actions...");
     }
+            //--Room--
     // Method to add a new room
     @Override
     public void addRoom(Room room){
-        System.out.println("Adding a new room...");
          HotelDatabase.rooms.add(room);
          //rooms is the arraylist of room in the database class, will be fixed when database class is finised
     }
     // Method to remove a room
     @Override
     public void deleteRoom(Room room){
-        System.out.println("Removing a room...");
         HotelDatabase.rooms.remove(room);
         //rooms is the arraylist of room in the database class, will be fixed when database class is finised
     }
@@ -40,10 +39,10 @@ public class Admin extends Staff implements Manageable {
             return;
         }
     }
-
-    System.out.println("Room not found");
 }
+
     // Method to read room information
+    @Override
     public void readRoom(int roomNumber) {
     for (int i = 0; i < HotelDatabase.rooms.size(); i++) {
         Room r = HotelDatabase.rooms.get(i);
@@ -55,7 +54,79 @@ public class Admin extends Staff implements Manageable {
         }
     }
 
-    System.out.println("Room not found");
+  
 }
+        //--Amenity--
+@Override
+public void addAmenity(Amenity amenity) {
+    HotelDatabase.amenities.add(amenity);
+    System.out.println("Amenity added");
+    //amenities is the arraylist of amenities in the database class, will be fixed when database class is finised
 
 }
+@Override
+public void deleteAmenity(Amenity amenity) {
+    HotelDatabase.amenities.remove(amenity);
+    System.out.println("Amenity deleted");
+
+    //amenities is the arraylist of amenities in the database class, will be fixed when database class is finised
+
+}
+    @Override
+    public void updateAmenity(String oldName, String newName) {
+        for (int i = 0; i < HotelDatabase.amenities.size(); i++) {
+        Amenity a = HotelDatabase.amenities.get(i);
+        //getName and setName are methods in the other classes that will be implemented when the classes are finished
+        if (a.getName().equals(oldName)) {
+            a.setName(newName);
+            System.out.println("Amenity updated");
+            return;
+        }
+    }
+     @Override
+    public void viewAmenities() {
+        for (int i = 0; i < HotelDatabase.amenities.size(); i++) {
+            System.out.println(HotelDatabase.amenities.get(i).getName());
+        }
+    }
+
+
+
+    // --RoomType--
+    @Override
+    public void addRoomType(RoomType t) {
+        HotelDatabase.roomTypes.add(t);
+    }
+
+    @Override
+    public void viewRoomTypes() {
+        for (int i = 0; i < HotelDatabase.roomTypes.size(); i++) {
+            System.out.println(HotelDatabase.roomTypes.get(i).getName());
+        }
+    }
+
+    @Override
+    public void updateRoomType(String oldName, String newName) {
+        for (int i = 0; i < HotelDatabase.roomTypes.size(); i++) {
+            RoomType t = HotelDatabase.roomTypes.get(i);
+
+            if (t.getName().equals(oldName)) {
+                t.setName(newName);
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void deleteRoomType(String name) {
+        for (int i = 0; i < HotelDatabase.roomTypes.size(); i++) {
+            if (HotelDatabase.roomTypes.get(i).getName().equals(name)) {
+                HotelDatabase.roomTypes.remove(i);
+                return;
+            }
+        }
+    }
+}
+}
+
+
